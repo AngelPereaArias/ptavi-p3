7a935a20e9b90a365d3fbc5047d48a65712c24f5
Angel <angel.pereaarias@gmail.com>
2016-10-08 12:14:16 +0200

    Created class and def 2 procedures with the final print

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,14 +7,30 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.General = {"root-layout" : ["width", "height", "background_color"],
+        self.trunk = []
+        self.General = {"root-layout" : ["width", "height", "background-color"],
                     "region" : ["id", "top", "bottom", "left", "right"],
                     "img" : ["src", "region", "begin", "dur"],
                     "audio" : ["src", "begin", "dur"],
                     "textstreams" : ["src", "region"]}
+
+
+    def startElement(self, name, attrs):
+
+        if name in self.General:
+            Value_Box = {}
+            for i in self.General[name]:
+                Value_Box[i] = attrs.get(i, "Empty attrs")
+
+            General_Slice = {name : Value_Box}
+            self.trunk.append(General_Slice)
+
+    def get_tags(self):
+        return self.trunk
 
 if __name__ == "__main__":
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open("karaoke.smil"))
+    print(cHandler.get_tags())
