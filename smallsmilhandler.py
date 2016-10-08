a5c2cfcc2232817d986ab7b06b86e9ec0403987e
Angel <angel.pereaarias@gmail.com>
2016-10-08 11:48:22 +0200

    created smallsmillhandler.py and initial commit

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+        self.General = {"root-layout" : ["width", "height", "background_color"],
+                    "region" : ["id", "top", "bottom", "left", "right"],
+                    "img" : ["src", "region", "begin", "dur"],
+                    "audio" : ["src", "begin", "dur"],
+                    "textstreams" : ["src", "region"]}
+
+if __name__ == "__main__":
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open("karaoke.smil"))
