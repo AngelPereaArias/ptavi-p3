b0107980b12c5a5bc243449558f351dd91031c92
Angel <angel.pereaarias@gmail.com>
2016-10-10 18:07:23 +0200

    Implemented object oriented procedures and class

--- karaoke.py
+++ karaoke.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -
 
 import sys
 import json
@@ -9,49 +9,65 @@ from xml.sax.handler import ContentHandler
 from urllib.request import urlretrieve
 
 
+class KaraokeLocal(SmallSMILHandler):
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.Trunk = cHandler.get_tags()
 
-def Create_Json(doc):
-	'''
-	Rewrites the doc collection into a .json file
-	'''
+    def do_local(self):
+        '''
+        Downloads the files contained on the URL,s
+        '''
 
-	json_file = open(sys.argv[1][:-5] + ".json", "w")
-	json.dump(doc, json_file)
-	json_file.close()
+        doc = ""
+        for dict_trunk in self.Trunk:
+            for key in dict_trunk:
+                doc = doc + key + "\t"
+                for key_2 in dict_trunk[key]:
+                    if str(dict_trunk[key][key_2])[0:7] == "http://":
+                        url = dict_trunk[key][key_2]
+                        dict_trunk[key][key_2] = url[url.rfind("/")+1:]
+                        urlretrieve(url)
 
-def Download_File(url):
-	'''
-	Downloads the files contained on the URL's
-	'''
+    def to_json(self, File, File_Name = ""):
+        '''
+        Rewrites the Trunk collection into a .json file
+        '''
+        if File_Name:
+        	File = File_Name
+        json_file = open(File, "w")
+        json.dump(self.Trunk, json_file, separators=(',', ': '), indent=4)
+        json_file.close()
 
-	print(url)
-	urlretrieve(url)
+    def __str__(self):
 
-def Format_Trunk(Trunk):
-	'''
-	Gives format to the collection "Trunk" 
-	Elemento1\tAtributo11="Valor11"\tAtributo12="Valor12"\t...\n
-	'''
+        '''
+        Gives format to the collection "Trunk"
+        Elemento1\tAtributo11="Valor11"\tAtributo12="Valor12"\t...\n
+        '''
 
-	doc = ""
-	for dict_trunk in Trunk:
-		for key in dict_trunk:
-			doc = doc + key + "\t"
-			for key_2 in dict_trunk[key]:
-				if key_2 == "src" and str(dict_trunk[key][key_2])[0:7] == "http://":
-					Download_File(dict_trunk[key][key_2])
-				doc = doc + key_2 + '="' + dict_trunk[key][key_2] + '"' + "\t"
-			doc = doc + "\n"
-	return doc
+        doc = ""
+        for dict_trunk in self.Trunk:
+            for key in dict_trunk:
+                doc = doc + key + "\t"
+                for key_2 in dict_trunk[key]:
+                    doc = doc + key_2 + '="' + dict_trunk[key][key_2] + '"' + "\t"
+                doc = doc + "\n"
+        print(doc)
+        return(doc)
 
 if __name__ == "__main__":
-
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
     try:
-    	parser.parse(open(sys.argv[1]))     
+        fichero = sys.argv[1]
+        Fich = KaraokeLocal(fichero)
     except:
         sys.exit("Usage: python3 karaoke.py file.smil")
-
-    Create_Json(Format_Trunk(cHandler.get_tags()))
+    Fich_Json = sys.argv[1][:-5] + ".json"
+    Fich.__str__()
+    Fich.to_json(Fich_Json)
+    Fich.do_local()
+    Fich.to_json(Fich_Json, "local.json")
+    Fich.__str__()
